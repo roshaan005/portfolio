4466b4779f29df16831370d9787250e7d73f3db6
anxious gritt <roshanjalil609@gmail.com>
2022-07-07 11:12:45 +0500

    changed the default export type

--- pages/components/NavElements.js
+++ pages/components/NavElements.js
@@ -1,9 +1,41 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Contact from '../Contact'
-import NavEl, { ArticleTag, ContactTag, ProjectsTag } from './NavEl'
 
 
+
+function ArticleTag({func,width}) {
+    
+
+  return (
+    <span onClick={func} className='group mb-6 text-gray-300'>
+    <span className='text-sm font-semibold text-gray-500'>01</span> 
+    <span className={`transition-s  mb-1 inline-block w-${width} h-px mx-2 -mt-px group-hover:w-10 bg-gray-500 group-hover:bg-white transition`}></span> 
+    <Link href='/'>Projects</Link>
+    </span>
+    
+  )
+}
+function ProjectsTag({func,width}) {
+    return (
+        <span onClick={func} className='group mb-6 text-gray-300'>
+        <span className='text-sm font-semibold text-gray-500'>02</span> 
+        <span className={`transition-s  mb-1 inline-block w-${width} h-px mx-2 -mt-px group-hover:w-10 bg-gray-500 group-hover:bg-white transition`}></span> 
+        <Link href='/Articles'>Articles</Link>
+        </span>
+    )
+  }
+
+  function ContactTag({func,width}) {
+    return (
+        <span onClick={func} className='group mb-6 text-gray-300'>
+        <span className='text-sm font-semibold text-gray-500'>03</span> 
+        <span className={`transition-s  mb-1 inline-block w-${width} h-px mx-2 -mt-px group-hover:w-10 bg-gray-500 group-hover:bg-white transition`}></span> 
+        <Link href='/Contact'>Contact</Link>
+        </span>
+    )
+  }
+  
 function NavElements() {
   const [status,setStatus] = useState({projects:"5",articles:"5",contact:"5"})
    const changeProjectStatus = ()=>{
