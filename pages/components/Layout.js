1f685c7c88ccde6ee58f7e4c7407e9dd03b58330
anxious gritt <roshanjalil609@gmail.com>
2022-11-09 21:45:53 +0500

    changed text

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -15,7 +15,7 @@ function Layout() {
          <div className='w-5/6 mt-10 md:mt-32 mb-10 h-fit lg:flex lg:flex-col lg:justify-center lg:items-center '>
            <span className='text-white font-bold text-5xl md:text-7xl'>Roshan <span className='text-gradient'> Jalil</span></span>
            <span className='mt-3 text-white  text-xl lg:text-2xl block'>Web Developer. Writer-ish. Student</span>
-           <div className=' mt-6  lg:w-8/12 set-padding  w-full  lg:mt-10 block text-gray-400 text-sm'>I am a high school student at the moment, coding for about 3 years. I have a knack for web tecnologies and anything related to coding and mathematics. I enjoy working with <span>React JS</span> and <span>Tailwind CSS</span>. I also have a blog :) </div>
+           <div className=' mt-6  lg:w-8/12 set-padding  w-full  lg:mt-10 block text-gray-400 text-sm'>I am a high school student at the moment, working as a part-time web-developer. I have a knack for web tecnologies and anything related to coding and mathematics. I enjoy working with <span>React JS</span> and <span>Tailwind CSS</span>. I also have a blog :) </div>
            <NavElements/>
            <Socials/>
     
